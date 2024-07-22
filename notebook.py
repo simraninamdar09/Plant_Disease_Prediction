c42886fdb176d86d5f91b7d33e4acffa354ee436
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-07-22 07:28:22 +0530

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -7,9 +7,9 @@ def model_prediction(test_image):
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr=tf.keras.preprocessing.image.img_to_array(image)
     input_arr=np.array([input_arr])# convert single img to batch
-    prediction = model.predict(input_arr)
-    result_index = np.argmax(prediction)
-    return result_index
+    predictions = model.predict(input_arr)
+    return np.argmax(predictions)
+    
 
 #Sidebar
 st.sidebar.title("Dashboard")
@@ -21,16 +21,26 @@ if(app_mode == "Home"):
     image_path = "Home.jpg"
     st.image(image_path,use_column_width=True)
     st.markdown("""
-                #### About Dataset
-                This dataset is recreated using offline augmentation from the original dataset.The original dataset can be found on this github repo.
-                This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes.The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure.
-                A new directory containing 33 test images is created later for prediction purpose.
-                #### Content
-                1. train (70295 images)
-                2. test (33 images)
-                3. validation (17572 images)
+    Welcome to the Plant Disease Recognition System! 🌿🔍
+    
+    Our mission is to help in identifying plant diseases efficiently. Upload an image of a plant, and our system will analyze it to detect any signs of diseases. Together, let's protect our crops and ensure a healthier harvest!
 
-                """)
+    ### How It Works
+    1. **Upload Image:** Go to the **Disease Recognition** page and upload an image of a plant with suspected diseases.
+    2. **Analysis:** Our system will process the image using advanced algorithms to identify potential diseases.
+    3. **Results:** View the results and recommendations for further action.
+
+    ### Why Choose Us?
+    - **Accuracy:** Our system utilizes state-of-the-art machine learning techniques for accurate disease detection.
+    - **User-Friendly:** Simple and intuitive interface for seamless user experience.
+    - **Fast and Efficient:** Receive results in seconds, allowing for quick decision-making.
+
+    ### Get Started
+    Click on the **Disease Recognition** page in the sidebar to upload an image and experience the power of our Plant Disease Recognition System!
+
+    ### About Us
+    Learn more about the project, our team, and our goals on the **About** page.
+    """)
 
 
 #About Page
@@ -54,7 +64,7 @@ elif(app_mode == "Disease Recognition"):
     st.header("Disease Recognition")
     test_image = st.file_uploader("Choose as Image:")
     if(st.button("Show Image")):
-        st.image(test_image,use_column_width=True)
+        st.image(test_image,width = 4,use_column_width=True)
 #Predict Button
     if(st.button("Predict")):
          st.write("Our Prediction")
