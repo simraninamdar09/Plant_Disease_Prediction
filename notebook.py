12cefa21f2259054a787a3395988e09fd920a932
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-07-22 07:21:22 +0530

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -4,7 +4,7 @@ import numpy as np
 # Tensorflow Model Prediction
 def model_prediction(test_image):
     model = tf.keras.models.load_model("training_model.keras")
-    image = tf.keras.preprocessing.image.load_img(image_path,target_size=(128,128))
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr=tf.keras.preprocessing.image.img_to_array(image)
     input_arr=np.array([input_arr])# convert single img to batch
     prediction = model.predict(input_arr)
